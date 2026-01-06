4c6347ef59dbffc9472d947ddab6e959e13ff80e
Emir Tucic <emir.tucic@gmail.com>
2026-01-06 23:21:41 +0100

    code formating

--- src/index.ts
+++ src/index.ts
@@ -12,10 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Handlebars setup
-app.engine('handlebars', engine({
-    layoutsDir: path.join(__dirname, "views", "layouts"),
-    partialsDir: path.join(__dirname, "views", "partials")
-}));
+app.engine(
+  'handlebars',
+  engine({
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+  })
+);
 
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
@@ -23,27 +26,32 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET || 'random-secret',
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
-        maxAge: 24 * 60 * 60 * 1000
-    }
-}));
+      secure: false,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
 app.use('/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist')));
-app.use('/bootstrap-icons', express.static(path.join(__dirname, '../node_modules/bootstrap-icons/font')));
+app.use(
+  '/bootstrap-icons',
+  express.static(path.join(__dirname, '../node_modules/bootstrap-icons/font'))
+);
 
 // Global state for all views
 app.use((req: Request, res: Response, next: NextFunction) => {
-    res.locals.isAuthenticated = !!req.session.userId;
-    next();
-})
+  res.locals.isAuthenticated = !!req.session.userId;
+  next();
+});
 
 app.get('/', (req: Request, res: Response) => {
-    res.redirect('/todos');
+  res.redirect('/todos');
 });
 
 app.use('/auth', authRoutes);
@@ -52,5 +60,5 @@ app.use('/todos', todoRoutes);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
